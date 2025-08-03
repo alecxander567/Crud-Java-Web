40d67cacfbe0ea993897701944682ed4e05bba1f
Mister Beans <162329514+alecxander567@users.noreply.github.com>
2025-08-03 16:51:50 +0800

    Create render-start.sh

---
+++ render-start.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Install SDKMAN to manage Java versions
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+# Install Java
+sdk install java 17.0.8-tem
+sdk use java 17.0.8-tem
+
+# Set JAVA_HOME
+export JAVA_HOME="$HOME/.sdkman/candidates/java/current"
+export PATH="$JAVA_HOME/bin:$PATH"
+
+# Start Spring Boot
+./mvnw spring-boot:run
